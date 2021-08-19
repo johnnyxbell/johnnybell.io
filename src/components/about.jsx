f42193403bc00890ce72c560c647efa0fac484b1
Johnny Bell <hi@johnnybell.io>
2021-08-19 14:39:15 -0700

    Update about.jsx

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -156,18 +156,6 @@ const About = () => {
   return (
     <AboutPanel>
       <AboutInfo>
-        <AboutInfoCurrent>
-          Currently working at{' '}
-          <a
-            href='http://weedmaps.com'
-            aria-label='Weedmaps'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Weedmaps
-          </a>{' '}
-          in <strong>Irvine, California</strong>.
-        </AboutInfoCurrent>
         <AboutInfoPast>
           I've been coding the internet for the past 10 years and working all over the world. Over
           that time, I’ve managed people, been a tech lead; now, I love being an individual
